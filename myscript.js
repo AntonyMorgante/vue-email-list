86a9c0582ea4428736da0fe0468bc196e929af54
AntonyMorgante <antonymorgante1@gmail.com>
2022-01-25 15:08:02 +0100

    script

---
+++ myscript.js
@@ -0,0 +1,22 @@
+let miapp = new Vue({
+    el: '#container',
+    data () {
+      return {
+        a:[]
+      }
+    },
+    mounted () {
+        for (let i=0;i<10;i++){
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => (this.a.push(response)))
+        }
+    }
+  })
+
+/* Descrizione:
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
